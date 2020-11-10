9a4a82f506f3a1f84f2e2586b52facf230fb8562
Fabio de Albuquerque Dela Antonio <fabio914@gmail.com>
2020-11-10 01:48:27 +0000

    Removing Checkouts folder

--- Carthage/Checkouts/SwiftSocket/Sources/SwiftSocket.h
+++
@@ -1,9 +0,0 @@
-@import Foundation;
-
-//! Project version number for SwiftSocket
-FOUNDATION_EXPORT double SwiftSocketVersionNumber;
-
-//! Project version string for SwiftSocket
-FOUNDATION_EXPORT const unsigned char SwiftSocketVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <SwiftSocket_iOS/PublicHeader.h>
