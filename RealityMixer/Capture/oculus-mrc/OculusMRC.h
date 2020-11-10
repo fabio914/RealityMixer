6e9cafa754f177a4aaf24d63a76561e6619f8e3a
gmarques33 <giovannimarques33@gmail.com>
2020-11-10 21:27:38 +0000

    Adding audio frames to OculusMRCDelegate

--- RealityMixer/Capture/oculus-mrc/OculusMRC.h
+++ RealityMixer/Capture/oculus-mrc/OculusMRC.h
@@ -12,8 +12,28 @@ NS_ASSUME_NONNULL_BEGIN
 
 @class OculusMRC;
 
+enum speaker_layout {
+    SPEAKERS_MONO,
+    SPEAKERS_STEREO
+};
+
+enum audio_format {
+    AUDIO_FORMAT_FLOAT
+};
+struct SourceAudio {
+    const uint8_t *data;
+    uint32_t frames;
+
+    enum speaker_layout speakers;
+    enum audio_format format;
+    uint32_t samples_per_sec;
+
+    uint64_t timestamp;
+};
+
 @protocol OculusMRCDelegate <NSObject>
 - (void)oculusMRC:(OculusMRC *)oculusMRC didReceiveImage:(UIImage *)image;
+- (void)oculusMRC:(OculusMRC *)oculusMRC didReceiveAudio:(struct SourceAudio *)audio;
 @end
 
 @interface OculusMRC : NSObject
