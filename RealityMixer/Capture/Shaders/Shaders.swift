6997612982636cdf2843ace2ffbf9460e3df47b0
Fabio de Albuquerque Dela Antonio <fabio914@gmail.com>
2021-07-08 21:33:10 +0100

    [Experiment] Silhouette effect

--- RealityMixer/Capture/Shaders/Shaders.swift
+++ RealityMixer/Capture/Shaders/Shaders.swift
@@ -126,7 +126,6 @@ struct Shaders {
         vec2 chroma = texture2D(u_diffuseTexture, _surface.diffuseTexcoord).rg;
 
         vec4 textureColor = yCbCrToRGB(luma, chroma);
-        _surface.diffuse = textureColor;
 
         float blendValue = smoothChromaKey(textureColor.rgb, maskColor, sensitivity, smoothness);
 
@@ -134,9 +133,11 @@ struct Shaders {
         _surface.ambient = vec4(1.0, 1.0, 1.0, 1.0);
 
         if (maskTextureValue > 0.5) {
-            _surface.transparent = vec4(blendValue, blendValue, blendValue, 1.0);
+            _surface.transparent = vec4(1.0 - blendValue, 1.0 - blendValue, 1.0 - blendValue, 1.0);
+            _surface.diffuse = vec4(1.0, 0.0, 1.0, 1.0);
         } else {
-            _surface.transparent = vec4(1.0, 1.0, 1.0, 1.0);
+            _surface.transparent = vec4(0.0, 0.0, 0.0, 1.0);
+            _surface.diffuse = vec4(1.0, 0.0, 1.0, 1.0);
         }
         """
     }
